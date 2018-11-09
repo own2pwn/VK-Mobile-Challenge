2be04e0555ee4eb33bec739f1a0c691e04f4ee8d
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-11-09 12:14:53 +0300

    +project

---
+++ VK-Challenge/VK-Challenge/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  VK-Challenge
+//
+//  Created by Evgeniy on 09.11.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ VK-Challenge/VK-Challenge/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  VK-Challenge
+//
+//  Created by Evgeniy on 09.11.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {}
