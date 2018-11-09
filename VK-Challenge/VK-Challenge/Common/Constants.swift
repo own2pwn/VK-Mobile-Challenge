1231106f6e2e3eb11fdf1f27d8530cc4c94dfa56
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-11-09 13:58:32 +0300

    auth process

---
+++ VK-Challenge/VK-Challenge/Common/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  VK-Challenge
+//
+//  Created by Evgeniy on 09.11.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import Foundation
+
+enum CONST {
+    enum VK {
+        static let appID = "6746442"
+    }
+}
---
+++ VK-Challenge/VK-Challenge/Extension/VK+Scope.swift
@@ -0,0 +1,16 @@
+//
+//  VK+Scope.swift
+//  VK-Challenge
+//
+//  Created by Evgeniy on 09.11.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import Foundation
+
+enum VKScope: String {
+    case wall
+    case photos
+    case friends
+    case offline
+}
