99ae820f188f08bf70ec1cbfacac5215c089b6ed
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-11-11 15:41:14 +0300

    search bar delegate

--- VK-Challenge/VK-Challenge/Module/Feed/View/FeedHeader.swift
+++ VK-Challenge/VK-Challenge/Module/Feed/View/FeedHeader.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
+protocol FeedHeaderDelegate: class {
+    func header(_ header: FeedHeader, didEnter text: String)
+}
+
 final class FeedHeader: UICollectionReusableView {
+    // MARK: - Interface
+
+    weak var delegate: FeedHeaderDelegate?
+
     // MARK: - Outlets
+
+    @IBOutlet
+    private var searchBar: UISearchBar!
 
     @IBOutlet
     private var avatarImageView: UIImageView!
@@ -20,11 +31,29 @@ final class FeedHeader: UICollectionReusableView {
         avatarImageView.image = image
     }
 
+    // MARK: - Setup
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        searchBar.delegate = self
+    }
+
     // MARK: - Layout
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
         avatarImageView.layer.cornerRadius = avatarImageView.frame.height / 2
+    }
+}
+
+extension FeedHeader: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("new text: \(searchText)")
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("end 2")
     }
 }
